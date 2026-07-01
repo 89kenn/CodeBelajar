331b957b16fef66fbf134e122dda8a3d7b8f40db
89kenn <riopramana016@gmail.com>
2026-07-01 23:38:46 +0700

    Debug Gemini response

--- api/chat.js
+++ api/chat.js
@@ -43,6 +43,8 @@ export default async function handler(req, res) {
 
     const data = await response.json();
 
+    console.log(JSON.stringify(data, null, 2));
+
     const answer =
       data?.candidates?.[0]?.content?.parts?.[0]?.text ||
       "Maaf, CodeAI belum mendapat jawaban dari Gemini.";
